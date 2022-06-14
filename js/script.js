00465404582c0870f15483dce61fa059b616287b
Zarina <zarina_nugmanova@mail.ru>
2022-06-14 18:31:49 -0400

    2nd hw changes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
-const levelForm = document.querySelector('.level');
+const levelForm = document.querySelector('#level');
+
 const levelButtons = levelForm.querySelectorAll('.level__choice');
-const levelSubmit = levelForm.querySelectorAll('.level__start')
 
-levelButtons.forEach(button => {
-    button.addEventListener('click', (event) => {
-        event.preventDefault();
-        console.log(button.value);
+const levelChoices = levelForm.querySelector('#level__choices');
+const levelSubmit = levelForm.querySelectorAll('#level__start')
 
-        window.application.level = button.value;
-    })
+// levelButtons.forEach(button => {
+//     button.addEventListener('click', (event) => {
+//         event.preventDefault();
+//         console.log(button.value);
+
+//         window.application.level = button.value;
+//     })
+// });
+
+levelChoices.addEventListener('click', (event) => {
+    event.preventDefault();
+    target = event.target
+    if (target.closest('.level__choice')) {
+        console.log(target.value);
+
+        window.application.level = target.value;
+    }
 });
 
 levelForm.addEventListener('submit', (event) => {
@@ -16,6 +29,9 @@ levelForm.addEventListener('submit', (event) => {
         event.preventDefault();
         levelButtons.forEach(button => {
             button.style.border = '1px solid red';
-        })
+        });
     }
-})
+
+    console.log(window.application);
+});
+
