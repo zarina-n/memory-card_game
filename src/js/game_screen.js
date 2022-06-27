fb9d9108ac2f51005c5ad838c839552b9968ce34
Zarina <zarina_nugmanova@mail.ru>
2022-06-27 18:13:11 -0400

    redone game logic, fixed issue with css and img

--- src/js/game_screen.js
+++ src/js/game_screen.js
@@ -1,137 +1,50 @@
 const cardsField = document.getElementById('cards-field')
 
-let index
+const cover = { imgSrc: './static/img/card-suit.jpg', name: 'back' }
 
-function randomNumber(min, max) {
-    return Math.ceil(Math.random() * (max - min) + min)
-}
-
-const cardArray = [
-    'card_1',
-    'card_2',
-    'card_3',
-    'card_4',
-    'card_5',
-    'card_6',
-    'card_7',
-    'card_8',
-    'card_9',
-    'card_10',
-    'card_11',
-    'card_12',
-    'card_13',
-    'card_14',
-    'card_15',
-    'card_16',
-    'card_17',
-    'card_18',
-    'card_19',
-    'card_20',
-    'card_21',
-    'card_22',
-    'card_23',
-    'card_24',
-    'card_25',
-    'card_26',
-    'card_27',
-    'card_28',
-    'card_29',
-    'card_30',
-    'card_31',
-    'card_32',
-    'card_33',
-    'card_34',
-    'card_35',
-    'card_36',
+const cardData = [
+    { imgSrc: './static/img/cards/card_1.jpg', name: 'card_1' },
+    { imgSrc: './static/img/cards/card_2.jpg', name: 'card_2' },
+    { imgSrc: './static/img/cards/card_3.jpg', name: 'card_3' },
+    { imgSrc: './static/img/cards/card_4.jpg', name: 'card_4' },
+    { imgSrc: './static/img/cards/card_5.jpg', name: 'card_5' },
+    { imgSrc: './static/img/cards/card_6.jpg', name: 'card_6' },
+    { imgSrc: './static/img/cards/card_7.jpg', name: 'card_7' },
+    { imgSrc: './static/img/cards/card_8.jpg', name: 'card_8' },
+    { imgSrc: './static/img/cards/card_9.jpg', name: 'card_9' },
+    { imgSrc: './static/img/cards/card_10.jpg', name: 'card_10' },
+    { imgSrc: './static/img/cards/card_11.jpg', name: 'card_11' },
+    { imgSrc: './static/img/cards/card_12.jpg', name: 'card_12' },
+    { imgSrc: './static/img/cards/card_13.jpg', name: 'card_13' },
+    { imgSrc: './static/img/cards/card_14.jpg', name: 'card_14' },
+    { imgSrc: './static/img/cards/card_15.jpg', name: 'card_15' },
+    { imgSrc: './static/img/cards/card_16.jpg', name: 'card_16' },
+    { imgSrc: './static/img/cards/card_17.jpg', name: 'card_17' },
+    { imgSrc: './static/img/cards/card_18.jpg', name: 'card_18' },
+    { imgSrc: './static/img/cards/card_19.jpg', name: 'card_19' },
+    { imgSrc: './static/img/cards/card_20.jpg', name: 'card_20' },
+    { imgSrc: './static/img/cards/card_21.jpg', name: 'card_21' },
+    { imgSrc: './static/img/cards/card_22.jpg', name: 'card_22' },
+    { imgSrc: './static/img/cards/card_23.jpg', name: 'card_23' },
+    { imgSrc: './static/img/cards/card_24.jpg', name: 'card_24' },
+    { imgSrc: './static/img/cards/card_25.jpg', name: 'card_25' },
+    { imgSrc: './static/img/cards/card_26.jpg', name: 'card_26' },
+    { imgSrc: './static/img/cards/card_27.jpg', name: 'card_27' },
+    { imgSrc: './static/img/cards/card_28.jpg', name: 'card_28' },
+    { imgSrc: './static/img/cards/card_29.jpg', name: 'card_29' },
+    { imgSrc: './static/img/cards/card_30.jpg', name: 'card_30' },
+    { imgSrc: './static/img/cards/card_31.jpg', name: 'card_31' },
+    { imgSrc: './static/img/cards/card_32.jpg', name: 'card_32' },
+    { imgSrc: './static/img/cards/card_33.jpg', name: 'card_33' },
+    { imgSrc: './static/img/cards/card_34.jpg', name: 'card_34' },
+    { imgSrc: './static/img/cards/card_35.jpg', name: 'card_35' },
+    { imgSrc: './static/img/cards/card_36.jpg', name: 'card_36' },
 ]
 
-function createCard(container) {
-    const cardContainer = document.createElement('div')
-    cardContainer.classList.add('card__inner')
-
-    const cardFront = document.createElement('img')
-    const cardCover = document.createElement('img')
-
-    cardContainer.appendChild(cardFront)
-    cardContainer.appendChild(cardCover)
-
-    cardFront.classList.add('card_front', 'card-face')
-    cardCover.classList.add('card_back', 'card-face')
-
-    cardCover.src = './src/img/card-suit.jpg'
-    cardFront.src = `./src/img/numbered_cards/${cardArray[index]}.jpg`
-
-    cardContainer.setAttribute('data-name', cardArray[index])
-
-    cardContainer.style.order = Math.floor(Math.random() * 10)
-
-    container.appendChild(cardContainer)
-
-    cardContainer.addEventListener('click', (event) => {
-        cardContainer.classList.toggle('is-flipped')
-        console.log(event.target.name)
-
-        const clickedCard = document.querySelectorAll('.is-flipped')
-        console.log(clickedCard)
-        console.log(clickedCard[0].getAttribute('data-name'))
-
-        if (clickedCard.length === 2) {
-            matchCards(clickedCard[0], clickedCard[1])
-        }
-    })
-
-    showCard(cardContainer)
-}
-
-function showCard(card) {
-    card.classList.toggle('is-flipped')
-
-    setTimeout(() => {
-        card.classList.toggle('is-flipped')
-    }, 5000)
-}
-
-function matchCards(cardOne, cardTwo) {
-    if (
-        cardOne.getAttribute('data-name') === cardTwo.getAttribute('data-name')
-    ) {
-        setTimeout(() => {
-            alert('your score')
-            cardOne.classList.toggle('is-flipped')
-            cardTwo.classList.toggle('is-flipped')
-        }, 2000)
-    } else {
-        setTimeout(() => {
-            alert('try again')
-            cardOne.classList.toggle('is-flipped')
-            cardTwo.classList.toggle('is-flipped')
-        }, 2000)
-    }
-}
-
-function renderLevelScreen(level) {
-    if (level === 9) {
-        cardsField.style.gridTemplateColumns = 'repeat(6, 1fr)'
-    }
-    if (level === 6) {
-        cardsField.style.gridTemplateColumns = 'repeat(4, 1fr)'
-    } else if (level === 3) {
-        cardsField.style.gridTemplateColumns = 'repeat(3, 1fr)'
-    }
-
-    for (let i = 0; i < level; i++) {
-        index = randomNumber(0, 35)
-
-        createCard(cardsField)
-        for (let j = 0; j < 1; j++) {
-            createCard(cardsField)
-        }
-    }
-}
+let levelNumber
 
 function getLevelNumber() {
     let level = localStorage.getItem('level')
-    let levelNumber
 
     if (level === 'easy') {
         levelNumber = 3
@@ -146,5 +59,104 @@ function getLevelNumber() {
     return levelNumber
 }
 
-console.log(getLevelNumber())
-renderLevelScreen(getLevelNumber())
+function randomize() {
+    cardData.sort(() => Math.random() - 0.5)
+
+    const newArr = cardData.slice(1, levelNumber + 1)
+    const duplicate = [...newArr]
+    const finalCardField = newArr.concat(duplicate)
+
+    finalCardField.sort(() => Math.random() - 0.5)
+
+    return finalCardField
+}
+
+function checkCards(event) {
+    const clickedCard = event.target
+
+    clickedCard.classList.add('clicked')
+    const clickedCards = document.querySelectorAll('.clicked')
+
+    if (clickedCards.length === 2) {
+        if (
+            clickedCards[0].getAttribute('name') ===
+            clickedCards[1].getAttribute('name')
+        ) {
+            setTimeout(() => {
+                alert('A score!')
+            }, 1000)
+
+            clickedCards.forEach((card) => {
+                card.classList.remove('clicked')
+                card.style.pointerEvents = 'none'
+            })
+        } else {
+            setTimeout(() => {
+                alert('try again')
+            }, 1000)
+
+            clickedCards.forEach((card) => {
+                card.classList.remove('clicked')
+                setTimeout(() => {
+                    card.classList.remove('flipCard')
+                }, 1000)
+            })
+        }
+    }
+}
+
+function showCard(card) {
+    card.classList.toggle('flipCard')
+
+    setTimeout(() => {
+        card.classList.toggle('flipCard')
+    }, 4000)
+}
+
+function cardGenerator() {
+    const cards = randomize()
+
+    cards.forEach((item) => {
+        const card = document.createElement('div')
+        const face = document.createElement('img')
+        const back = document.createElement('img')
+
+        card.classList.add('card')
+        face.classList.add('face')
+        back.classList.add('back')
+
+        card.setAttribute('name', item.name)
+
+        cardsField.appendChild(card)
+        card.appendChild(face)
+        card.appendChild(back)
+
+        face.src = item.imgSrc
+        back.src = cover.imgSrc
+
+        showCard(card)
+
+        card.addEventListener('click', (event) => {
+            card.classList.add('flipCard')
+
+            checkCards(event)
+        })
+    })
+}
+
+function createCardBlock() {
+    getLevelNumber()
+
+    if (levelNumber === 9) {
+        cardsField.style.gridTemplateColumns = 'repeat(6, 1fr)'
+    }
+    if (levelNumber === 6) {
+        cardsField.style.gridTemplateColumns = 'repeat(4, 1fr)'
+    } else if (levelNumber === 3) {
+        cardsField.style.gridTemplateColumns = 'repeat(3, 1fr)'
+    }
+
+    cardGenerator()
+}
+
+createCardBlock()
--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,3 @@
 import '../css/style.css'
 import './game_screen'
-
-const levelForm = document.getElementById('level')
-
-const levelChoiceButtons = levelForm.querySelectorAll('.level__choice')
-
-const levelChoices = document.getElementById('level__choices')
-
-let clicked = false
-
-levelChoices.addEventListener('click', (event) => {
-    event.preventDefault()
-    const { target } = event
-
-    if (target.closest('button')) {
-        console.log(target.dataset.level)
-
-        localStorage.setItem('level', target.dataset.level)
-
-        let level = localStorage.getItem('level')
-
-        clicked = true
-
-        console.log(level)
-    }
-})
-
-levelForm.addEventListener('submit', (event) => {
-    if (clicked === 'false') {
-        event.preventDefault()
-        levelChoiceButtons.forEach((levelButton) => {
-            levelButton.style.border = '1px solid red'
-        })
-    }
-})
+import './level_choice'
